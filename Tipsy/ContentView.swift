9fb511f34d53d51bd908ed3d8a70fedae786bd9e
Abhishek Saral <abhisheksaral17@gmail.com>
2020-01-01 15:07:57 -0800

    Initial Commit

---
+++ Tipsy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tipsy
+//
+//  Created by Abhishek Saral on 1/1/20.
+//  Copyright © 2020 Tech Knowns. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
